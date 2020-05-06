4a796e65448b55a027b48d946c72268db6408942
Administrator <rajarsi3997@gmail.com>
2020-05-06 20:01:40 +0530

    Daily auto updates

---
+++ commit.sh
@@ -0,0 +1,6 @@
+cd /c/Users/Administrator/Documents/GitHub/Covid19_Daily_Updates
+git checkout master
+git add --all
+git commit -am "Daily auto updates"
+git push https://github.com/RajarsiGit/Covid19_Daily_Updates.git master
+exit
