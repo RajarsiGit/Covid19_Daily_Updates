814140b7a6f3fa86b96f3be8cffc612cb9f660b8
Rajarsi Saha <rajarsi3997@gmail.com>
2020-05-13 10:53:44 +0530

    Daily auto updates

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 git checkout master
 git add --all
 git commit -am "Daily auto updates"
-git push https://github.com/RajarsiGit/Covid19_Daily_Updates.git master
+git push https://RajarsiGit:RajarsiSaha%403997@github.com/RajarsiGit/Covid19_Daily_Updates.git master
 exit
