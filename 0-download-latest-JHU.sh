2a68db9b1cbe1d9cc0ab8287bd8e8c2e3d0ed1b6
Administrator <rajarsi3997@gmail.com>
2020-05-17 14:53:44 +0530

    Daily auto updates

--- 0-download-latest-JHU.sh
+++ 0-download-latest-JHU.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rm -rf Covid19_Daily_Updates/data
+rm -rf data
 git clone --depth=1 https://github.com/CSSEGISandData/COVID-19.git Covid19_Daily_Updates/data
-rm -rf Covid19_Daily_Updates/data/.git
+rm -rf data/.git
